cd1b738297d03ff6ecc749d9945a0ad5acb82686
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-14 15:16:42 +0000

    Changed ToDouble to ToInt32

--- main.cs
+++ main.cs
@@ -5,7 +5,7 @@ class Program {
     const double InchesToCentimetersInteractive = 2.54;
 
     Console.WriteLine("Enter the amount of inches to calculate to centimeters: ");
-    double inches = Convert.ToDouble(Console.ReadLine());
+    double inches = Convert.ToInt32(Console.ReadLine());
     double cent = inches * InchesToCentimetersInteractive;
     
     Console.WriteLine ("{0} inches is {1} centimeters", inches, cent);
