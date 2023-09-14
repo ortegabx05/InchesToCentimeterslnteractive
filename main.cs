8b14c47ceaec89cccb82b6d11335caca71395239
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-14 15:10:39 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    const double InchesToCentimetersInteractive = 2.54;
+
+    Console.WriteLine("Enter the amount of inches to calculate to centimeters: ");
+    double inches = Convert.ToDouble(Console.ReadLine());
+    double cent = inches * InchesToCentimetersInteractive;
+    
+    Console.WriteLine ("{0} inches is {1} centimeters", inches, cent);
+  }
+}
